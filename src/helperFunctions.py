0567bf4494dbeac73941980e701b797e130800e9
Niklas Hübel <niklashubel@niklass-mbp.localdomain>
2021-02-04 11:15:26 +0100

    helperFunction and full preprocessing pipeline

--- src/helperFunctions.py
+++ src/helperFunctions.py
@@ -64,9 +64,9 @@ def mean_imputation(df, columns, enforce_int=False):
     - df: modified dataframe
 
     Example:
-    df =  mean_imputation(df, ['column_A'])
-    df =  mean_imputation(df, ['column_A'], enforce_int=True)
-    df =  mean_imputation(df, ['column_A','column_B'])
+    df = mean_imputation(df, ['column_A'])
+    df = mean_imputation(df, ['column_A'], enforce_int=True)
+    df = mean_imputation(df, ['column_A','column_B'])
     """
     if enforce_int:
         mean_imp_values = round(df[columns].mean())
@@ -74,6 +74,32 @@ def mean_imputation(df, columns, enforce_int=False):
         mean_imp_values = df[columns].mean()
 
     df = df.fillna(mean_imp_values, axis=0)
+    return df
+
+
+def holiday_imputation(df, holidays):
+    """
+    Description:
+    Imputes na values in 'StateHoliday' column with 'a'/'0' if 'Date' is/is not a national German holiday
+
+    Input:
+    - df: dataframe
+    - holidays: python holidays package
+
+    Output:
+    - df: modified dataframe
+
+    Example:
+    import holidays
+    ...
+    df = holiday_imputation(df, holidays)
+    """
+    years = df.Date.dt.year.unique()
+    national_holidays = [day for day in holidays.Germany(years=years)]
+    missing_holiday = df.StateHoliday.isnull()
+    holiday_date = df.Date.isin(national_holidays)
+    df.loc[missing_holiday & holiday_date, 'StateHoliday'] = 'a'
+    df.loc[missing_holiday & ~holiday_date, 'StateHoliday'] = '0'
     return df
 
 
@@ -99,7 +125,7 @@ def freq_encoding(df, column):
     """
     fenc_values = df[column].value_counts()
     new_column = column + '_fenc'
-    df[new_column] = df[column].map(fenc_values)
+    df.loc[:, new_column] = df[column].map(fenc_values)
     return df, fenc_values
 
 def mean_encoding(df, column):
@@ -119,7 +145,8 @@ def mean_encoding(df, column):
     df, menc_values = mean_encoding(df, 'column_A')
     """
     menc_values = df.groupby(by = column).mean()['Sales']
-    df[column + '_menc'] = df[column].map(menc_values)
+    new_column = column + '_menc'
+    df.loc[:, new_column] = df[column].map(menc_values)
     return df, menc_values
 
 def ordinal_encoding(df, column, ordinal_dict):
@@ -138,5 +165,27 @@ def ordinal_encoding(df, column, ordinal_dict):
     Example:
     df = ordinal_encoding(df, 'column_A', {'large': 3, 'medium': 2, 'small': 1})
     """
-    df[column + '_orenc'] = df[column].map(ordinal_dict)
+    new_column = column + '_orenc'
+    df[new_column] = df[column].map(ordinal_dict)
+    return df
+
+def binary_encoding(df, column, positive_list):
+    """
+    Description:
+    Adds a column to a dataframe that contains the binary encoding of a given feature
+
+    Input:
+    - df: dataframe
+    - column: column name as string
+    - positive_list: list containing values to be encoded as one
+
+    Output:
+    - df: modified dataframe
+
+    Example:
+    df = ordinal_encoding(df, 'column_A', ['category_A', 'category_B', 'category_C'])
+    """
+    new_column = column + '_benc'
+    df[new_column] = 0
+    df.loc[df[column].isin(positive_list), new_column] = 1
     return df
